f800bc597c9dd6ff71313f92e47fd3425144cef6
Eugen <admin@locarus.ru>
2017-09-14 17:20:06 +0500

    Added bubbleSort and some CI files

---
+++ sort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+#  Python example of using sorting algorithm and git flow for working with source code. Source is here:
+#
+#  https://github.com/evgenvs/py_sort_gitflow
+#
+
+
+
+def bubbleSort(alist):
+    for nums in range(len(alist)-1,0,-1):
+        for i in range(nums):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+    return alist
+
+
